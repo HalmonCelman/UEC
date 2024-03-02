0cc46ffb8554318d514dea0ca299a9464d1f5d57
Halmon <halmoncelman@gmail.com>
2024-03-02 16:22:05 +0100

    Some assertions
    - idk if it's correct

--- sim/vga_timing/vga_timing_tb.sv
+++ sim/vga_timing/vga_timing_tb.sv
@@ -75,14 +75,25 @@ vga_timing dut(
  */
 
  // Here you can declare tasks with immediate assertions (assert).
+task hcount_initialized;
 
+endtask;
 
 /**
  * Assertions
  */
 
 // Here you can declare concurrent assertions (assert property).
+assert property (@(posedge clk) ((hcount === 'x) || (hcount < HOR_TOTAL_TIME))) else $error("hcount out of range");
+assert property (@(posedge clk) ((vcount === 'x) || (vcount < VER_TOTAL_TIME))) else $error("vcount out of range");
 
+// hlbnk check
+assert property (@(posedge clk) ((hcount >= HOR_BLANK_START && hcount <= HOR_BLANK_START + HOR_BLANK_TIME) |-> hblnk == 1));
+assert property (@(posedge clk) ((hcount < HOR_BLANK_START || hcount > HOR_BLANK_START + HOR_BLANK_TIME) |-> (hblnk == 0 || hblnk === 'x))) else $error("hblnk isn't going down");
+
+// hsync check
+assert property (@(posedge clk) ((hcount >= HOR_SYNC_START && hcount <= HOR_SYNC_START + HOR_SYNC_TIME) |-> hsync == 1)) else $error("hsync isn't going up");
+assert property (@(posedge clk) ((hcount < HOR_SYNC_START || hcount > HOR_SYNC_START + HOR_SYNC_TIME) |-> (hsync == 0 || hsync === 'x))) else $error("hsync isn't going down");
 
 /**
  * Main test
@@ -91,6 +102,8 @@ vga_timing dut(
 initial begin
     @(posedge rst);
     @(negedge rst);
+
+
 
     wait (vsync == 1'b0);
     @(negedge vsync)
