45277b675e3fb04beabfdc9239d4882c8cc42a9b
Halmon <halmoncelman@gmail.com>
2024-04-03 17:34:09 +0200

    all simulations working

--- sim/vga_timing/vga_timing_tb.sv
+++ sim/vga_timing/vga_timing_tb.sv
@@ -28,10 +28,7 @@ localparam CLK_PERIOD = 25;     // 40 MHz
 logic clk;
 logic rst;
 
-wire [10:0] vcount, hcount;
-wire        vsync,  hsync;
-wire        vblnk,  hblnk;
-
+vga_if vga();
 
 /**
  * Clock generation
@@ -62,12 +59,7 @@ end
 vga_timing dut(
     .clk,
     .rst,
-    .vcount,
-    .vsync,
-    .vblnk,
-    .hcount,
-    .hsync,
-    .hblnk
+    .vga
 );
 
 /**
@@ -77,12 +69,12 @@ vga_timing dut(
 
 bit reseted;
 task signals_initialized;
-    assert(hcount == '0)    else $error("hcount is not 0 at start");
-    assert(vcount == '0)    else $error("vcount is not 0 at start");
-    assert(hblnk == '0)     else $error("hblnk is not 0 at start");
-    assert(vblnk == '0)     else $error("vblnk is not 0 at start");
-    assert(hsync == '0)     else $error("hsync is not 0 at start");
-    assert(vsync == '0)     else $error("vsync is not 0 at start");
+    assert(vga.hcount == '0)    else $error("hcount is not 0 at start");
+    assert(vga.vcount == '0)    else $error("vcount is not 0 at start");
+    assert(vga.hblnk == '0)     else $error("hblnk is not 0 at start");
+    assert(vga.vblnk == '0)     else $error("vblnk is not 0 at start");
+    assert(vga.hsync == '0)     else $error("hsync is not 0 at start");
+    assert(vga.vsync == '0)     else $error("vsync is not 0 at start");
 endtask;
 
 /**
@@ -91,24 +83,24 @@ endtask;
 
 // Here you can declare concurrent assertions (assert property).
 // counters check
-assert property (@(posedge clk) ((rst == 1) || (reseted == 0) || (hcount < HOR_TOTAL_TIME))) else $error("hcount out of range");
-assert property (@(posedge clk) ((rst == 1) || (reseted == 0) || (vcount < VER_TOTAL_TIME))) else $error("vcount out of range");
+assert property (@(posedge clk) ((rst == 1) || (reseted == 0) || (vga.hcount < HOR_TOTAL_TIME))) else $error("hcount out of range");
+assert property (@(posedge clk) ((rst == 1) || (reseted == 0) || (vga.vcount < VER_TOTAL_TIME))) else $error("vcount out of range");
 
 // hblnk check
-assert property (@(posedge clk) ((hcount >= HOR_BLANK_START && hcount < HOR_BLANK_START + HOR_BLANK_TIME) |-> ( hblnk == 1 || rst == 1 || reseted == 0))) else $error("hblnk isn't going up");
-assert property (@(posedge clk) ((hcount < HOR_BLANK_START || hcount > HOR_BLANK_START + HOR_BLANK_TIME) |-> (hblnk == 0 || rst == 1 || reseted == 0))) else $error("hblnk isn't going down");
+assert property (@(posedge clk) ((vga.hcount >= HOR_BLANK_START && vga.hcount < HOR_BLANK_START + HOR_BLANK_TIME) |-> ( vga.hblnk == 1 || rst == 1 || reseted == 0))) else $error("hblnk isn't going up");
+assert property (@(posedge clk) ((vga.hcount < HOR_BLANK_START || vga.hcount > HOR_BLANK_START + HOR_BLANK_TIME)  |-> ( vga.hblnk == 0 || rst == 1 || reseted == 0))) else $error("hblnk isn't going down");
 
 // hsync check
-assert property (@(posedge clk) ((hcount >= HOR_SYNC_START && hcount < HOR_SYNC_START + HOR_SYNC_TIME) |-> ( hsync == 1 || rst == 1 || reseted == 0))) else $error("hsync isn't going up");
-assert property (@(posedge clk) ((hcount < HOR_SYNC_START || hcount > HOR_SYNC_START + HOR_SYNC_TIME) |-> (hsync == 0 || rst == 1 || reseted == 0))) else $error("hsync isn't going down");
+assert property (@(posedge clk) ((vga.hcount >= HOR_SYNC_START && vga.hcount < HOR_SYNC_START + HOR_SYNC_TIME) |-> ( vga.hsync == 1 || rst == 1 || reseted == 0))) else $error("hsync isn't going up");
+assert property (@(posedge clk) ((vga.hcount < HOR_SYNC_START || vga.hcount > HOR_SYNC_START + HOR_SYNC_TIME)  |-> ( vga.hsync == 0 || rst == 1 || reseted == 0))) else $error("hsync isn't going down");
 
 // vblnk check
-assert property (@(posedge clk) ((vcount >= VER_BLANK_START && vcount < VER_BLANK_START + VER_BLANK_TIME) |-> ( vblnk == 1 || rst == 1 || reseted == 0))) else $error("vblnk isn't going up");
-assert property (@(posedge clk) ((vcount < VER_BLANK_START || vcount > VER_BLANK_START + VER_BLANK_TIME) |-> (vblnk == 0 || rst == 1 || reseted == 0))) else $error("vblnk isn't going down");
+assert property (@(posedge clk) ((vga.vcount >= VER_BLANK_START && vga.vcount < VER_BLANK_START + VER_BLANK_TIME) |-> ( vga.vblnk == 1 || rst == 1 || reseted == 0))) else $error("vblnk isn't going up");
+assert property (@(posedge clk) ((vga.vcount < VER_BLANK_START || vga.vcount > VER_BLANK_START + VER_BLANK_TIME)  |-> ( vga.vblnk == 0 || rst == 1 || reseted == 0))) else $error("vblnk isn't going down");
 
 // vsync check
-assert property (@(posedge clk) ((vcount >= VER_SYNC_START && vcount < VER_SYNC_START + VER_SYNC_TIME) |-> ( vsync == 1 || rst == 1 || reseted == 0))) else $error("vsync isn't going up");
-assert property (@(posedge clk) ((vcount < VER_SYNC_START || vcount > VER_SYNC_START + VER_SYNC_TIME) |-> (vsync == 0 || rst == 1 || reseted == 0))) else $error("vsync isn't going down");
+assert property (@(posedge clk) ((vga.vcount >= VER_SYNC_START && vga.vcount < VER_SYNC_START + VER_SYNC_TIME) |-> ( vga.vsync == 1 || rst == 1 || reseted == 0))) else $error("vsync isn't going up");
+assert property (@(posedge clk) ((vga.vcount < VER_SYNC_START || vga.vcount > VER_SYNC_START + VER_SYNC_TIME)  |-> ( vga.vsync == 0 || rst == 1 || reseted == 0))) else $error("vsync isn't going down");
 
 
 /**
@@ -125,9 +117,9 @@ initial begin
 
     signals_initialized();
 
-    wait (vsync == 1'b0);
-    @(negedge vsync)
-    @(negedge vsync)
+    wait (vga.vsync == 1'b0);
+    @(negedge vga.vsync)
+    @(negedge vga.vsync)
 
     $finish;
 end
