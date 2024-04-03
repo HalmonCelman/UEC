8d30beeb11c7db0550586375f3770ce377402662
Halmon <halmoncelman@gmail.com>
2024-04-03 20:22:15 +0200

    Propably working first part of lab2_3

--- fpga/rtl/top_vga_basys3.sv
+++ fpga/rtl/top_vga_basys3.sv
@@ -29,11 +29,9 @@ module top_vga_basys3 (
 /**
  * Local variables and signals
  */
-
-wire clk_in, clk_fb, clk_ss, clk_out;
 wire locked;
-wire pclk;
-wire pclk_mirror;
+wire clk100MHz, clk40MHz;
+wire clk40MHz_mirror;
 
 (* KEEP = "TRUE" *)
 (* ASYNC_REG = "TRUE" *)
@@ -46,65 +44,27 @@ logic [7:0] safe_start = 0;
  * Signals assignments
  */
 
-assign JA1 = pclk_mirror;
+assign JA1 = clk40MHz_mirror;
 
 
 /**
  * FPGA submodules placement
  */
 
-IBUF clk_ibuf (
-    .I(clk),
-    .O(clk_in)
-);
+  clk_wiz_0_clk_wiz clk_wiz
+  (
+  .clk100MHz,
+  .clk40MHz,       
+  .locked,
+  .clk
+  );
 
-MMCME2_BASE #(
-    .CLKIN1_PERIOD(10.000),
-    .CLKFBOUT_MULT_F(10.000),
-    .CLKOUT0_DIVIDE_F(25.000)
-) clk_in_mmcme2 (
-    .CLKIN1(clk_in),
-    .CLKOUT0(clk_out),
-    .CLKOUT0B(),
-    .CLKOUT1(),
-    .CLKOUT1B(),
-    .CLKOUT2(),
-    .CLKOUT2B(),
-    .CLKOUT3(),
-    .CLKOUT3B(),
-    .CLKOUT4(),
-    .CLKOUT5(),
-    .CLKOUT6(),
-    .CLKFBOUT(clk_fb),
-    .CLKFBOUTB(),
-    .CLKFBIN(clk_fb),
-    .LOCKED(locked),
-    .PWRDWN(1'b0),
-    .RST(1'b0)
-);
-
-BUFH clk_out_bufh (
-    .I(clk_out),
-    .O(clk_ss)
-);
-
-always_ff @(posedge clk_ss)
-    safe_start <= {safe_start[6:0],locked};
-
-BUFGCE #(
-    .SIM_DEVICE("7SERIES")
-) clk_out_bufgce (
-    .I(clk_out),
-    .CE(safe_start[7]),
-    .O(pclk)
-);
-
-// Mirror pclk on a pin for use by the testbench;
+// Mirror clk40MHz on a pin for use by the testbench;
 // not functionally required for this design to work.
 
 ODDR pclk_oddr (
-    .Q(pclk_mirror),
-    .C(pclk),
+    .Q(clk40MHz_mirror),
+    .C(clk40MHz),
     .CE(1'b1),
     .D1(1'b1),
     .D2(1'b0),
@@ -118,7 +78,7 @@ ODDR pclk_oddr (
  */
 
 top_vga u_top_vga (
-    .clk(pclk),
+    .clk(clk40MHz),
     .rst(btnC),
     .r(vgaRed),
     .g(vgaGreen),
